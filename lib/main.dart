32db2fe146ebb4d80f8c6219b59244f42707c4ec
darpanrewani05 <darpanrewani@gmail.com>
2025-07-08 15:04:40 +0530

    First Push

---
+++ lib/main.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic Tac Toe',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: TicTacToeGame(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class TicTacToeGame extends StatefulWidget {
+  @override
+  _TicTacToeGameState createState() => _TicTacToeGameState();
+}
+
+class _TicTacToeGameState extends State<TicTacToeGame> {
+  List<String> board = List.filled(9, '');
+  String currentPlayer = 'X';
+  String winner = '';
+
+  void resetGame() {
+    setState(() {
+      board = List.filled(9, '');
+      currentPlayer = 'X';
+      winner = '';
+    });
+  }
+
+  void playMove(int index) {
+    if (board[index] == '' && winner == '') {
+      setState(() {
+        board[index] = currentPlayer;
+        winner = checkWinner();
+        if (winner != '') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ResultScreen(
+                result: winner == 'Draw' ? "It's a Draw!" : "Winner: $winner",
+                onNewGame: () {
+                  resetGame();
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          );
+        } else {
+          currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
+      });
+    }
+  }
+
+  String checkWinner() {
+    List<List<int>> winPositions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (var pos in winPositions) {
+      if (board[pos[0]] != '' &&
+          board[pos[0]] == board[pos[1]] &&
+          board[pos[1]] == board[pos[2]]) {
+        return board[pos[0]];
+      }
+    }
+
+    if (!board.contains('')) {
+      return 'Draw';
+    }
+
+    return '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final boardSize = size.width < size.height ? size.width : size.height;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Tic Tac Toe',),
+      centerTitle: true,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purpleAccent, Colors.deepPurple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: boardSize * 0.9,
+              height: boardSize * 0.9,
+              padding: EdgeInsets.all(10),
+              child: GridView.builder(
+                itemCount: 9,
+                gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: () => playMove(index),
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: Center(
+                      child: Text(
+                        board[index],
+                        style: TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: board[index] == 'X' ? Colors.blue : Colors.red,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Current Player: $currentPlayer',
+              style: TextStyle(fontSize: 24, color: Colors.white),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: resetGame,
+              child: Text('Reset Game'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ResultScreen extends StatelessWidget {
+  final String result;
+  final VoidCallback onNewGame;
+
+  ResultScreen({required this.result, required this.onNewGame});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.lightBlue, Colors.blueGrey],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                result,
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: onNewGame,
+                child: Text('New Game'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
